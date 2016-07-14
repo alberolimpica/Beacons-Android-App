9115e8dc2cbdb886af853cd45fe177ae98646887
R00143659 <s.alberola-mahiques@mycit.ie>
2016-07-14 10:26:00 +0100

    Uploading the blank beacon app

---
+++ app/src/main/java/com/example/r00143659/beacondeployment/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.r00143659.beacondeployment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
