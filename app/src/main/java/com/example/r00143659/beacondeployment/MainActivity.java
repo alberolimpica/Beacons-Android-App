8ed021b2c450a7726d8afebc21a8ba9b41a82a9d
alberolimpica <salma4@alumni.uv.es>
2016-07-19 12:44:31 +0100

    Beacon app after the tuorial of the page redirected by starkoverflow

--- app/src/main/java/com/example/r00143659/beacondeployment/MainActivity.java
+++ app/src/main/java/com/example/r00143659/beacondeployment/MainActivity.java
@@ -1,13 +1,100 @@
 package com.example.r00143659.beacondeployment;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+
+public class MainActivity  extends AppCompatActivity implements BeaconConsumer, RangeNotifier {
+
+    private BeaconManager mBeaconManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+
+    @Override
+    //This method gets called when the activity appears
+    public  void onResume (){
+        super.onResume();
+        mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+        //setBeaconLayout method will tell the Android Beacon Library how to decode an Eddystone UID frame
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"));
+        mBeaconManager.bind(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onBeaconServiceConnect(){
+        //To tell the libraryy that we want to see all beacons:
+        Region region = new Region("all-beacons-region", null, null, null);
+        try{
+            //to start looking for beacons that match this region definition
+            mBeaconManager.startRangingBeaconsInRegion(region);
+        }catch(RemoteException e){
+            e.printStackTrace();
+        }
+        //This class will receive callbacks everytime a beacon is seen
+        mBeaconManager.setRangeNotifier(this);
+    }
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+        for (Beacon beacon: beacons) {
+            // You can tell if a beacon is an Eddystone beacon because it will have a serviceUuid of
+            // 0xfeaa, and a beaconTypeCode of x00. (For the Eddystone-TLM frame, the beaconTypeCode will be 0x20 and for Eddystone-URL the beaconType code will be 0x10).
+            if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x00) {
+                // This is a Eddystone-UID frame
+                Identifier namespaceId = beacon.getId1();
+                Identifier instanceId = beacon.getId2();
+                Log.d("RangingActivity", "I see a beacon transmitting namespace id: " + namespaceId +
+                        " and instance id: " + instanceId +
+                        " approximately " + beacon.getDistance() + " meters away.");
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        ((TextView)MainActivity.this.findViewById(R.id.message)).setText("Hello world, and welcome to Eddystone!");
+                    }
+                });
+            }
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mBeaconManager.unbind(this);
+    }
+
 }
+
+
+
+
