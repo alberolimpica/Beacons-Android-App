10e0e772dc9be57c24c8c8f861846f6ac56ed384
alberolimpica <salma4@alumni.uv.es>
2016-08-11 10:46:41 +0100

    Fixed the error. But now I don't have the toolbar/upper bar

--- app/src/main/java/com/example/r00143659/beacondeployment/MainActivity.java
+++ app/src/main/java/com/example/r00143659/beacondeployment/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.r00143659.beacondeployment;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +12,7 @@ import android.widget.Button;
  * Main Activity of the app.
  * Here, we will see two buttons to choose between finding a beacon or treasure hunting
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     Button button1, button2;
 
     @Override
--- app/src/main/java/com/example/r00143659/beacondeployment/TreasureHunting.java
+++ app/src/main/java/com/example/r00143659/beacondeployment/TreasureHunting.java
@@ -2,11 +2,12 @@ package com.example.r00143659.beacondeployment;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Treasure hunting activity. Here the stamps will be shown.
  */
-public class TreasureHunting extends Activity {
+public class TreasureHunting extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
